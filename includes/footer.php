f95ed171fdb32e93f49a67d14fa94d70e25fb549
KanenasO <160666361+KanenasO@users.noreply.github.com>
2024-03-13 18:59:36 +0200

    make sure to change to the actuall restaurant name

---
+++ includes/footer.php
@@ -0,0 +1,12 @@
+<!-- Footer -->
+<footer class="footer">
+    <div class="container">
+        <span>&copy; 2024 Restaurant Name. All rights reserved.</span>
+    </div>
+</footer>
+
+<script src="assets/js/jquery-3.5.1.min.js"></script>
+<script src="assets/js/bootstrap.bundle.min.js"></script>
+<script src="assets/js/script.js"></script>
+</body>
+</html>
