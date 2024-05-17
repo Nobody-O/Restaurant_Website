c458342061cdf08cdaaeee44ad567397ef00138c
KanenasO <160666361+KanenasO@users.noreply.github.com>
2024-05-17 17:13:54 +0300

    updated old

--- process-contact.php
+++ process-contact.php
@@ -1,17 +1,12 @@
 <?php
-session_start(); // Start the session to carry error messages
-
-// Include database configuration
+session_start(); 
 include 'includes/config.php';
 
-// Check if the form is submitted
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Retrieve form data and remove whitespace
     $name = trim($_POST["name"]);
     $email = trim($_POST["email"]);
     $message = trim($_POST["message"]);
 
-    // Basic validation
     if (empty($name)) {
         $_SESSION['error'] = 'Name is required.';
         header('Location: contact.php');
@@ -26,34 +21,24 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         exit();
     }
 
-    // Prepare SQL statement to insert form data into the 'contact_messages' table
     $sql = "INSERT INTO contact_messages (name, email, message) VALUES (:name, :email, :message)";
-    
     try {
         $stmt = $pdo->prepare($sql);
-        
-        // Bind parameters to statement
         $stmt->bindParam(':name', $name, PDO::PARAM_STR);
         $stmt->bindParam(':email', $email, PDO::PARAM_STR);
         $stmt->bindParam(':message', $message, PDO::PARAM_STR);
-        
-        // Execute the statement
         $stmt->execute();
 
-         // Redirect to a thank-you page with success message
-         $_SESSION['success'] = 'Your message has been successfully submitted.';
-         header('Location: thank-you.php');
-         exit();
-         
+        $_SESSION['success'] = 'Your message has been successfully submitted.';
+        header('Location: thank-you.php');
+        exit();
     } catch (PDOException $e) {
-        // Log the error and set an error message
         error_log($e->getMessage());
         $_SESSION['error'] = 'There was an error processing your message. Please try again.';
         header('Location: contact.php');
         exit();
     }
 } else {
-    // Set an error message for requests not using POST
     $_SESSION['error'] = 'There was a problem with your submission. Please try again.';
     header('Location: contact.php');
     exit();
--- sql/restaurant_db.sql
+++ sql/restaurant_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 16, 2024 at 12:00 AM
+-- Generation Time: May 17, 2024 at 04:12 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -104,7 +104,8 @@ CREATE TABLE `reservations` (
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
   `username` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL
+  `password` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
