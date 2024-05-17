40f770249fecb89dd30a6a9a3483e810aa875446
Nobody-O <127896860+Nobody-O@users.noreply.github.com>
2024-05-17 17:24:40 +0300

    Update process-register.php

--- process-register.php
+++ process-register.php
@@ -1,12 +1,12 @@
 <?php
-include('config.php');
+include('includes/config.php');
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $username = $_POST['username'];
-    $email = $_POST['email'];
+    $username = trim($_POST['username']);
+    $email = trim($_POST['email']);
     $password = password_hash($_POST['password'], PASSWORD_BCRYPT);
 
-    $stmt = $conn->prepare("INSERT INTO users (username, email, password) VALUES (:username, :email, :password)");
+    $stmt = $pdo->prepare("INSERT INTO users (username, email, password) VALUES (:username, :email, :password)");
     $stmt->bindParam(':username', $username);
     $stmt->bindParam(':email', $email);
     $stmt->bindParam(':password', $password);
